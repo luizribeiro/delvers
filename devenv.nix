f419bcd5f616c3b7383a3d44fa711eefccd5901a
Luiz Ribeiro <luizribeiro@gmail.com>
2026-04-16 22:59:55 -0400

    Add git hooks for fmt, clippy, and test
    Pre-commit runs rustfmt and clippy via devenv git-hooks.
    Pre-push runs cargo test via a shell script installed on shell entry.

--- devenv.nix
+++ devenv.nix
@@ -2,4 +2,13 @@
 
 {
   languages.rust.enable = true;
+
+  git-hooks.hooks = {
+    rustfmt.enable = true;
+    clippy.enable = true;
+  };
+
+  enterShell = ''
+    install -m 755 .githooks/pre-push .git/hooks/pre-push
+  '';
 }
